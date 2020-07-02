420dd90bf2bad54b27cc680f7bc69caeafd56952
Student_GA111 <egen03@gmail.com>
2020-07-02 11:14:54 -0400

    added psuedo code for html and css

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ console.log('working')
 // 1. Create classes for Player 1, Player 2, answering questions right or wrong*
 // 2. Create methods for getting question right or wrong.
 // 3. Create loops for multiple choice conditions
-// 4. Create way to log results
+// 4. Create way to log results to keep score
 // 5. Create way to alternate between players
 // 6. Create way to reset game
 
+// HTML
+
+// 1. Create layout gameboard using grid
+// 2. Insert buttons
+// 3. Insert theme music
+
+// CSS
+
+// 1. Design buttons with hover effect
+// 2. Animate right and wrong choice selections
