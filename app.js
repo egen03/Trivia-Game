fe9a5d487f6a18e913adff5a3c91628af5b54013
Student_GA111 <egen03@gmail.com>
2020-07-02 10:41:56 -0400

    psuedo code for javascript

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('working')
+
+// Psuedo Code
+
+// Javascript
+
+// 1. Create classes for Player 1, Player 2, answering questions right or wrong*
+// 2. Create methods for getting question right or wrong.
+// 3. Create loops for multiple choice conditions
+// 4. Create way to log results
+// 5. Create way to alternate between players
+// 6. Create way to reset game
+
